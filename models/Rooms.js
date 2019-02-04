52f603780d2890424c1565c995feac7f3656205a
Ameya Angal <ameyaangal@ameyas-mbp.attlocal.net>
2019-02-04 08:58:15 -0500

    room service api

---
+++ models/Rooms.js
@@ -0,0 +1,38 @@
+var db = require("../database/dbconnection");
+
+var rooms = {
+  getAllRooms: function(callback) {
+    console.log("Get all rooms");
+    return db.query("Select * from rooms;", callback);
+  },
+  getRoomById: function(id, callback) {
+    return db.query("Select * from rooms where Id=?;", [id], callback);
+  },
+  getRoomsByLocation: function(location, callback) {
+    return db.query(
+      "Select * from rooms where location = ?;",
+      [location],
+      callback
+    );
+  },
+  createRoom: function(Rooms, callback) {
+    console.log(Rooms);
+    return db.query(
+      "Insert into rooms  (location, owneremail, guests, available, price, description, wifi, microwave, safeCloset) values (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        Rooms.location,
+        Rooms.owneremail,
+        Rooms.guests,
+        Rooms.available,
+        Rooms.price,
+        Rooms.description,
+        Rooms.wifi,
+        Rooms.microwave,
+        Rooms.safeCloset
+      ],
+      callback
+    );
+  }
+};
+
+module.exports = rooms;
---
+++ routes/rooms.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+var room = require("../models/Rooms");
+
+router.get("/:id?", function(req, res, next) {
+  if (req.params.id) {
+    room.getRoomById(req.params.id, function(err, rows) {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(rows);
+      }
+    });
+  } else {
+    room.getAllRooms(function(err, rows) {
+      console.log(rows);
+      if (err) {
+        res.json(err);
+      } else {
+        console.log(rows);
+        res.json(rows);
+      }
+    });
+  }
+});
+
+router.post("/", function(req, res, next) {
+  console.log("request body");
+  console.log(req.body);
+  room.createRoom(req.body, function(err, count) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(req.body); //or return count for 1 &amp;amp;amp; 0
+    }
+  });
+});
+
+module.exports = router;
