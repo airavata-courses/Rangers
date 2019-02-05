a62c7f54894710d657f55eae678693a4f91677a1
rkasture <rohan.kasture2011@gmail.com>
2019-02-05 15:28:59 -0500

    Email Notify service

--- notify_user.py
+++ notify_user.py
@@ -1,25 +1,59 @@
-#Rocco Manzo
-#11/30/2018
 
 import sys
 import smtplib
+from flask import Flask,request
 from email.mime.text import MIMEText
+from flask_cors import CORS, cross_origin
+from configparser import SafeConfigParser
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 
-#takes a list of emails and sends a reminder
-def sendEmail(emails):
-    msg = MIMEText("Submit your evals by midnight.")
-    msg['Subject'] = "Submit Eval"
+parser = SafeConfigParser()
+parser.read('config.ini')
 
-    for email in emails:
-        msg['From'], msg['To'] = "oosdreminder@gmail.com", email
+MAIL_SERVER = "localhost.cs.indiana.edu"
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = "rentnleaseservice@gmail.com"
+MAIL_PASSWORD = "rentnlease#2019"
+MAIL_DEFAULT_SENDER = "rentnleaseservice@gmail.com"
+
+app = Flask(__name__)
+
+app.config["MAIL_SERVER"] = MAIL_SERVER
+app.config["MAIL_PORT"] = MAIL_PORT
+app.config["MAIL_USE_SSL"] = MAIL_USE_SSL
+app.config['MAIL_USE_TLS'] = False
+app.config["MAIL_DEFAULT_SENDER"] = MAIL_DEFAULT_SENDER
+
+@app.route('/', methods=['POST'])
+def sendEmail():
+    try:
+        post_data = request.get_json()
+        recipient = post_data.get('recipient')
+        subject = post_data.get('subject')
+        body = post_data.get('body')
+        sender = MAIL_USERNAME
+        msg = MIMEText(body)
+        msg['Subject'] = subject
+        msg['From'], msg['To'] = sender, recipient
         # Send the message via gmail SMTP server
         s = smtplib.SMTP('smtp.gmail.com', 587)
         #connect to the gmail server
         s.ehlo()
         s.starttls()
-        s.login(msg['From'], 'oosdisgreat')
+        s.login(MAIL_USERNAME, MAIL_PASSWORD)
         #TODO: make it parse email and pass from config.ini?
-        s.sendmail(msg['From'], [msg['To']], msg.as_string())
+        s.sendmail(MAIL_USERNAME, recipient, msg.as_string())
         s.quit()
+        return "Mail sent Successfully"
+    except Exception as e:
+        app.logger.error(e)
 
-sendEmail(['rkasture@iu.edu'])
+if __name__ == '__main__':
+    http_server = HTTPServer(WSGIContainer(app))
+
+    http_server.listen(55555)
+
+    IOLoop.instance().start()
