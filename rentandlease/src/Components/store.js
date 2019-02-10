b868a74f356265badbe739caa1f554aa3fb8175e
Ameya Angal <ameyaangal@149-160-174-239.dhcp-bl.indiana.edu>
2019-02-10 13:41:45 -0500

    Fixed the store issue

--- rentandlease/src/Components/store.js
+++ rentandlease/src/Components/store.js
@@ -10,12 +10,18 @@ const rootReducer = combineReducers({
   form: formReducer
 });
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const middleware = applyMiddleware(thunk);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(middleware));
+
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
