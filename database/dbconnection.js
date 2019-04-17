ba1e7367cddec50a5e8386165d6ca5fd714b0e18
cbrahme <42560156+cbrahme@users.noreply.github.com>
2019-04-17 19:44:28 -0400

    kubemaster IP

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var connection = mysql.createPool({
-    host: "db",
+    host: "149.165.171.144",
     user: "root",
     password: "password",
     database: "rangers",
