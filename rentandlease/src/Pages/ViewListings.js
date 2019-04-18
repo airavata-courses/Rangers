b63261ce04236278b939ca73a2d011b615b795a9
cbrahme <42560156+cbrahme@users.noreply.github.com>
2019-04-17 20:20:43 -0400

    changed to kubemaster IP

--- rentandlease/src/Pages/ViewListings.js
+++ rentandlease/src/Pages/ViewListings.js
@@ -25,7 +25,7 @@ export class ViewListings extends PureComponent {
       body: body
     };
     postApi(
-      "http://149.165.156.112:5001/notify",
+      "http://149.165.171.144:30004/notify",
       data => {
         console.log(data);
       },
