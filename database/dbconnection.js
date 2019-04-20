290077b322d5201bb97322afce684692db0c5325
Ameya Angal <ameyaangal@149-160-142-90.dhcp-bl.indiana.edu>
2019-04-19 22:25:36 -0400

    Modified node base image and dbconnection

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,9 +1,12 @@
 var mysql = require("mysql");
 var connection = mysql.createPool({
-    host: "db",
-    user: "root",
-    password: "password",
-    database: "rangers",
-    insecureAuth : true
+  host: "149.165.171.144",
+  user: "root",
+  password: "password",
+  database: "rangers",
+  insecureAuth: true,
+  port: "3306"
 });
 module.exports = connection;
+
+// host: "149.165.157.130",
